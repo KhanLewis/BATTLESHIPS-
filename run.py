61acf2f50a88e3c71b09da503973f3344e6e311d
KhanLewis <khanlewis@hotmail.co.uk>
2022-10-24 11:01:37 +0000

    addship location with import randint

--- run.py
+++ run.py
@@ -14,11 +14,28 @@ def create_board(users_input):
     return board
 
 
-def print_board(users_board):
+def print_board(board):
+    """"prints the board with spaces between"""
     for i in users_board:
         print(" ".join(i))
 
 
-users_board = create_board(int(input("battlefield must be between 4-8: ")))
+def random_row(board):
+    """
+    generates random row for the computers ship
+    """
+    return randint(0, len(users_board) - 1)
 
+def random_col(board):
+    """
+    generates random col for the computers ship
+    """
+    return randint(0, len(users_board[0]) - 1)
+
+
+print("WELCOME TO BATTLE SHIPS")
+print("please enter the size of your battle field")
+users_board = create_board(int(input("battlefield must be between 4-8: ")))
 print_board(users_board)
+ship_row = random_row(users_board)
+ship_col = random_col(users_board)
