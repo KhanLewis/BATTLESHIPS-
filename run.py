ce21b1f4aa07e237a49a82d9df721d224c782682
KhanLewis <khanlewis@hotmail.co.uk>
2022-10-24 10:02:38 +0000

    add a create board and print board function

--- run.py
+++ run.py
@@ -3,3 +3,22 @@ Welcome to my battleships game.
 """
 from random import randint
 
+
+def create_board(users_input):
+    """
+    creates a board depending on the users input
+    """
+    board = []
+    for i in range(users_input):
+        board.append(["~"]*users_input)
+    return board
+
+
+def print_board(users_board):
+    for i in users_board:
+        print(" ".join(i))
+
+
+users_board = create_board(int(input("battlefield must be between 4-8: ")))
+
+print_board(users_board)
